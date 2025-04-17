df28c0409a1b248e364ce1c52b86191a09de08c0
Quest <ecsolticia@gmail.com>
2025-04-17 06:28:49 +0600

    Fixed mathematical error in `Pencil`

--- Scenes/Pencil/pencil.gd
+++ Scenes/Pencil/pencil.gd
@@ -2,11 +2,6 @@ extends Line2D
 
 @export var drawing_node_path: NodePath
 
-func coordinate_inner_line2d() -> void:
-	$Inner.points = points
-
 func _physics_process(delta: float) -> void:
 	var drawing_node: Node2D = get_node(drawing_node_path)
-	add_point(drawing_node.global_position + drawing_node.line)
-	
-	coordinate_inner_line2d()
+	add_point(drawing_node.global_position + drawing_node.line * drawing_node.scale_lines)
